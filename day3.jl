de6256fe0352c524782130caee868fe9a2d90dba
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-12-04 16:19:38 -0500

    completed day3

--- day3.jl
+++ day3.jl
@@ -7,6 +7,7 @@ for (k,v) in zip(collect('A':'Z'), collect(27:52))
     mydict[k] = v
 end
 
+#Part 1
 open("day3.txt") do f
     letters = []
     score = 0
@@ -22,3 +23,19 @@ open("day3.txt") do f
     println("Part 1 total:")
     println(score)
 end
+
+#Part 2
+f = readlines("day3.txt")
+badges = []
+global part2score = 0
+num_groups = trunc(Int, length(f)/3)
+println(num_groups)
+for l in 0:(num_groups-1)
+    push!(badges,intersect(f[3*l+1], f[3*l+2], f[3*l+3])[1])
+end
+for i in 1:length(badges)
+    global part2score+=mydict[badges[i]]
+end
+println("Part 2 total:")
+println(part2score)
+#end
