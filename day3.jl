de2d3c2f5dff9064e13020e628ec616f0975e034
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-12-04 10:29:25 -0500

    day3 part 1 completed

--- day3.jl
+++ day3.jl
@@ -1,10 +1,24 @@
+using StatsBase
+
+alphabet = collect('a':'z')
+points = collect(1:26)
+mydict = Dict(alphabet .=> points)
+for (k,v) in zip(collect('A':'Z'), collect(27:52))
+    mydict[k] = v
+end
+
 open("day3.txt") do f
+    letters = []
+    score = 0
     for l in eachline(f)
         halflen = trunc(Int, length(l)/2)
-        println("check")
-        println(l)
         l1 = SubString(l, 1, halflen)
         l2 = SubString(l, halflen+1)
-        println((?l1)(.*)(?l2))
+        push!(letters, intersect(l1,l2)[1])
     end
+    for i in 1:length(letters)
+        score+=mydict[letters[i]]
+    end
+    println("Part 1 total:")
+    println(score)
 end
