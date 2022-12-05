d12681aacaf6d45ffceeb62d9e4ac1e648ec3c40
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-12-05 18:11:32 -0500

    day4 completed

--- day4.jl
+++ day4.jl
@@ -21,13 +21,26 @@ function string_contains(s, t)
     end
 end
 
+function overlaps(s, t)
+    if s[1]<=t[1]<=s[2]
+        return 1
+    elseif t[1]<=s[1]<=t[2]
+        return 1
+    else
+        return 0
+    end
+end
+
 open("day4.txt") do f
     counter = 0
+    overlap_counter = 0
     for l in eachline(f)
         (s, t) = splitstring(l)
         s_range = getrange(s)
         t_range = getrange(t)
         counter += string_contains(s_range, t_range)
+        overlap_counter += overlaps(s_range, t_range)
     end
     println("Part 1:", counter)
+    println("Part 2:", overlap_counter)
 end
