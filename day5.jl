34b70a99e83abc117a498ed4c491f1db71851e10
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-12-05 20:45:26 -0500

    day5 finished

--- day5.jl
+++ day5.jl
@@ -1,5 +1,5 @@
 f = readlines("day5.txt")
-global boxes = Dict(
+boxes = Dict(
     1 => ['R', 'G', 'H', 'Q', 'S', 'B', 'T', 'N'],
     2 => ['H', 'S', 'F', 'D', 'P', 'Z', 'J'],
     3 => ['Z', 'H', 'V'],
@@ -10,6 +10,9 @@ global boxes = Dict(
     8 => ['Q', 'V', 'W', 'S'],
     9 => ['W', 'H', 'L', 'M', 'T', 'D', 'N', 'C']
 )
+
+global multi_boxes = deepcopy(boxes)
+global single_boxes = deepcopy(boxes)
 
 function move_object(from_stack, to_stack, stacks::Dict)
     new_dict = stacks
@@ -22,20 +25,47 @@ function move_object(from_stack, to_stack, stacks::Dict)
     return(new_dict)
 end
 
+function move_stacks(from_stack, to_stack, num, stacks::Dict)
+    new_dict = stacks
+    from_temp = pop!(new_dict, from_stack)
+    to_temp = pop!(new_dict, to_stack)
+    append!(to_temp, last(from_temp, num))
+    n = length(from_temp)
+    deleteat!(from_temp, (n-num+1):n)
+    new_dict[from_stack] = from_temp
+    new_dict[to_stack] = to_temp
+    return(new_dict)
+end
+
+#Part 1
 num_moves = length(f)
-#moves = [filter(isdigit, collect(s)) for s in f[11:end]]
 for m in 11:num_moves
     strsplit = split(f[m], " ")
     num_iter = parse(Int, strsplit[2])
     from_stack = parse(Int, strsplit[4])
     to_stack = parse(Int, strsplit[6])
     for i in 1:num_iter
-        global boxes = move_object(from_stack, to_stack, boxes)
+        global single_boxes = move_object(from_stack, to_stack, single_boxes)
     end
 end
 
 println("Part 1: ")
 for j in 1:9
-    print(boxes[j][end])
+    print(single_boxes[j][end])
+end
+println(" ")
+
+#Part 2
+for m in 11:num_moves
+    strsplit = split(f[m], " ")
+    num_iter = parse(Int, strsplit[2])
+    from_stack = parse(Int, strsplit[4])
+    to_stack = parse(Int, strsplit[6])
+    global multi_boxes = move_stacks(from_stack, to_stack, num_iter, multi_boxes)
+end
+
+println("Part 2: ")
+for j in 1:9
+    print(multi_boxes[j][end])
 end
 println(" ")
