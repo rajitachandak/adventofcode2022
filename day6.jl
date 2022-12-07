44fe142c8db8df561e65951cbd2a90f622eb3c0a
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-12-06 21:41:58 -0500

    day6

---
+++ day6.jl
@@ -0,0 +1,21 @@
+f = readlines("day6.txt")[1]
+
+function marker(s::String, d::Int)
+    n = length(s) - d
+    marker_list = []
+    for i in 1:n
+        push!(marker_list, length(unique(s[i:(i+(d-1))])))
+    end
+    marker = findfirst(==(d), marker_list) + (d-1)
+    return (marker)
+end
+
+#Part 1
+println("Part 1: ")
+packet_marker = marker(f, 4)
+println(packet_marker)
+
+#Part 2
+println("Part 2: ")
+msg_marker = marker(f, 14)
+println(msg_marker)
