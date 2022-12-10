407fb99a924185c0a4c403d0b51e0e655ba5143a
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-12-10 15:43:01 -0500

    working on day 7

--- day7.jl
+++ day7.jl
@@ -1,21 +1,30 @@
 f = readlines("day7.txt")
 
 #find all dir strings
-function dir_loc(s)
-    dir_list = findall(x -> startswith(x, "dir"), s)
+function object_loc(s, word)
+    dir_list = findall(x -> startswith(x, word), s)
     return dir_list
 end
 #collect all directory names
-function dir_names(s)
-    dirs = [chop(x, head=4, tail=0) for x in s]
+function dir_names(s, l)
+    dirs = [chop(x, head=l, tail=0) for x in s]
     return (dirs)
 end
 
-locations = dir_loc(f)
-all_dirs = f[locations]
-dir_list = dir_names(all_dirs)
-println(dir_list)
-
+cd_loc = object_loc(f, r"\$ cd [a-z]")
+dir_loc = dir_names(f[cd_loc], 5)
+global mydict = Dict()
+for i in 1:length(cd_loc)-1
+    mydict[dir_loc[i]] = f[cd_loc[i]+2:cd_loc[i+1]-1]
+    dir_list = object_loc(mydict[dir_loc[i]], "dir")
+    println(dir_loc[i])
+    println(dir_list)
+    if isempty(dir_list)
+        println("not nested")
+    else
+    end
+end
+#println(mydict)
 # dictionary of dirs
 
 
