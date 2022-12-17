dc360d498c58b2f3ad61e2a019c88be38ec30d75
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-12-17 07:51:03 +0530

    trying out classes

--- day7.jl
+++ day7.jl
@@ -1,31 +1,24 @@
 f = readlines("day7.txt")
 
-#find all dir strings
-function object_loc(s, word)
-    dir_list = findall(x -> startswith(x, word), s)
-    return dir_list
-end
-#collect all directory names
-function dir_names(s, l)
-    dirs = [chop(x, head=l, tail=0) for x in s]
-    return (dirs)
+mutable struct Directory
+    name::String
+    parent::String
+    dir_size::Int
 end
 
-cd_loc = object_loc(f, r"\$ cd [a-z]")
-dir_loc = dir_names(f[cd_loc], 5)
-global mydict = Dict()
-for i in 1:length(cd_loc)-1
-    mydict[dir_loc[i]] = f[cd_loc[i]+2:cd_loc[i+1]-1]
-    dir_list = object_loc(mydict[dir_loc[i]], "dir")
-    println(dir_loc[i])
-    println(dir_list)
-    if isempty(dir_list)
-        println("not nested")
-    else
+mutable struct File
+    name::String
+    dir::Directory
+    file_size::Int
+end
+
+system = Directory("/", "", 0)
+println(system)
+
+for i in 1:length(f)
+    line = f[i]
+    if startswith(line, r"\$ cd [a-z]")
+#        println(chop(line, head=5, tail=0))
+#        i = Directory(chop(line, head=5, tail=0), i-1, 0)
     end
 end
-#println(mydict)
-# dictionary of dirs
-
-
-#
