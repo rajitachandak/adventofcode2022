8f795a6d9626a051c2198d87824b945db866a15d
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-12-17 10:06:51 +0530

    stuck with multiple dirnames

--- day7.jl
+++ day7.jl
@@ -9,25 +9,40 @@ end
 
 mutable struct File
     name::String
-    dir::Directory
+    parent_dir::String
     file_size::Int
 end
 
-function files(name, parent_dir, file_size)
+function new_file(name, parent_dir, file_size)
     name = File(name, parent_dir, file_size)
 end
 
-function dirs(name, parent, file_list, dir_size)
+function new_dir(name, parent, file_list, dir_size)
     name = Directory(name, parent, file_list, dir_size)
 end
 
-system = Directory("/", "", 0)
-println(system)
+#establishing file system
+global system = Dict("/" => Directory("/", "", Dict(), 0))
+global files = Dict()
+global dirs = Dict()
 
-for i in 1:length(f)
+#initialzing directories and files
+global parent = "/"
+global i = 1
+while i < length(f)+1
     line = f[i]
     if startswith(line, r"\$ cd [a-z]")
-#        println(chop(line, head=5, tail=0))
-#        i = Directory(chop(line, head=5, tail=0), i-1, 0)
+        global parent = chop(line, head=5, tail=0)
     end
+    if startswith(line, r"[0-9]")
+        l = split(line)
+        file_name = l[2]
+        global files[file_name] = new_file(l[2], parent, parse(Int, l[1]))
+    end
+    if startswith(line, r"dir")
+        l = split(line)
+        dir_name = l[2]
+        global dirs[dir_name] = new_dir(l[2], parent, Dict(), 0)
+    end
+    global i +=1
 end
