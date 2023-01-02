d0623532d7d71e50ebc04ab55f01c00245b6204c
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2023-01-02 11:41:00 +0530

    working on day 12

--- day12.jl
+++ day12.jl
@@ -16,8 +16,6 @@ function create_matrix(f::Vector{String})
                 f_mat[i, j] = 0
             elseif f[i][j] == 'E'
                 f_mat[i, j] = 27
-                println(f[i][j])
-                println(f_mat[i, j])
             else
                 f_mat[i, j] = mydict[f[i][j]]
             end
@@ -27,5 +25,51 @@ function create_matrix(f::Vector{String})
 end
 
 mat = create_matrix(f)
-display(mat)
-println()
+#display(mat)
+#println()
+
+function next_step(curr_row::Int, curr_col::Int, M::Matrix)
+    curr_val = M[curr_row, curr_col]
+
+    next_step = []
+    if curr_row>1 && curr_col>1
+        if abs(M[curr_row-1, curr_col] - curr_val)<= 1
+            push!(next_step, [curr_row-1, curr_col])
+        end
+        if abs(M[curr_row+1, curr_col] - curr_val) <=1
+            push!(next_step, [curr_row+1, curr_col])
+        end
+        if abs(M[curr_row, curr_col+1] - curr_val) <=1
+            push!(next_step, [curr_row, curr_col+1])
+        end
+        if abs(M[curr_row, curr_col-1] - curr_val) <=1
+            push!(next_step, [curr_row, curr_col-1])
+        end
+    else
+        if abs(M[curr_row+1, curr_col] - curr_val) <=1
+            push!(next_step, [curr_row+1, curr_col])
+        end
+        if abs(M[curr_row, curr_col+1] - curr_val) <=1
+            push!(next_step, [curr_row, curr_col+1])
+        end
+    end
+    return(next_step)
+end
+println(next_step(2, 2, mat))
+
+function map_path(M::Matrix{Int}, s::Int, e::Int)
+    (n, m) = size(M)
+
+    end_point = findall(x->x==e, M)
+    end_col = end_point[1][2]
+    end_row = end_point[1][1]
+
+    start_point = findall(x->x==s, M)
+    start_col = start_point[1][2]
+    start_row = start_point[1][1]
+
+    steps = 0
+
+end
+
+println(map_path(mat, 0, 27))
