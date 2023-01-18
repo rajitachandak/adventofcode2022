5606f3dbd8d29f3b60f5cae366b9eb08c8d9b468
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2023-01-18 12:04:52 -0500

    day12

--- day12.jl
+++ day12.jl
@@ -2,7 +2,7 @@ f = readlines("day12.txt")
 
 function create_matrix(f::Vector{String})
     alphabet = collect('a':'z')
-    points = collect(26:-1:1)
+    points = collect(1:26)
     mydict = Dict(alphabet .=> points)
 
     n = length(f)
@@ -12,9 +12,9 @@ function create_matrix(f::Vector{String})
     for i in 1:n
         for j in 1:m
             if f[i][j] == 'S'
-                f_mat[i, j] = 27
-            elseif f[i][j] == 'E'
                 f_mat[i, j] = 0
+            elseif f[i][j] == 'E'
+                f_mat[i, j] = 27
             else
                 f_mat[i, j] = mydict[f[i][j]]
             end
@@ -25,53 +25,84 @@ end
 
 mat = create_matrix(f)
 
-function update_dis!(curr_min, dis::Matrix, v_set::Vector)
-    M = create_matrix(f)
-    curr_val = curr_min[1]
-    curr_row = curr_min[2][1]
-    curr_col = curr_min[2][2]
+function get_neighbours(curr_min, n, m)
+    i = curr_min[1]
+    j = curr_min[2]
+    neighbours = []
 
-    if (M[curr_row-1, curr_col] - curr_val) == 1
-        dis[curr_row-1, curr_col] = curr_val + 1
-        push!(v_set, [curr_row-1, curr_col])
+    if i > 1
+        push!(neighbours, [i-1, j])
     end
-    if (M[curr_row+1, curr_col] - curr_val) ==1
-        dis[curr_row+1, curr_col] = curr_val + 1
-        push!(v_set, [curr_row+1, curr_col])
+
+    if i < n
+        push!(neighbours, [i+1, j])
     end
-    if (M[curr_row, curr_col+1] - curr_val) ==1
-        dis[curr_row, curr_col+1] = curr_val + 1
-        push!(v_set, [curr_row, curr_col+1])
+    if j > 1
+        push!(neighbours, [i, j-1])
     end
-    if (M[curr_row, curr_col-1] - curr_val) ==1
-        dis[curr_row, curr_col-1] = curr_val + 1
-        push!(v_set, [curr_row, curr_col-1])
+
+    if j < m
+        push!(neighbours, [i, j+1])
     end
-    return(dis, v_set)
+    return(neighbours)
 end
 
-function map_dis(M::Matrix{Int}, s::Int, e::Int)
+function dijkstra(M::Matrix{Int})
     (n, m) = size(M)
 
-    steps = 0
-    v_set = []
     dis = zeros(n, m)
+    curr_min = []
     for i in 1:n
         for j in 1:m
-            if M[i, j] == 0
+            if M[i, j] == 27
                 dis[i, j] = 0
+                curr_min = [i, j]
             else
                 dis[i, j] = Inf
             end
         end
     end
 
-    min_v = findmin(dis)
-    push!(v_set, [min_v[2][1], min_v[2][2]])
+    visited = fill(false, (n, m))
+    unvisited = fill(true, (n, m))
 
-    dis = update_dis!(min_v, dis, v_set)
+    while any(unvisited)
+        curr_min = []
+        for i in 1:n
+            for j in 1:m
+                if unvisited[i, j]
+                    if isempty(curr_min)
+                        curr_min = [i, j]
+                    elseif dis[i, j] < dis[curr_min[1], curr_min[2]]
+                        curr_min = [i, j]
+                    end
+                end
+            end
+        end
 
+        neighbours = get_neighbours(curr_min, n, m)
+        for (i,j) in neighbours
+            if M[curr_min[1], curr_min[2]] -  M[i, j] == 0
+                temp_val = dis[curr_min[1], curr_min[2]] + (M[curr_min[1], curr_min[2]] -  M[i, j] + 1)
+            else
+                temp_val = dis[curr_min[1], curr_min[2]] + (M[curr_min[1], curr_min[2]] -  M[i, j])
+            end
+            if temp_val < dis[i, j]
+                dis[i, j] = temp_val
+            end
+        end
+        visited[curr_min[1], curr_min[2]] = true
+        unvisited[curr_min[1], curr_min[2]] = false
+    end
     return(dis)
 end
 
-println(map_dis(mat, 0, 27))
+function min_distance(dis::Matrix, point::CartesianIndex{2})
+    return(dis[point[1], point[2]])
+end
+
+dis = dijkstra(mat)
+println(dis)
+s = findall(a->a ==0, mat)[1]
+println(s)
+println("Part 1:", min_distance(dis,s))
