38c8f2d7b450b565d4a7ee3a59e98ba2af620adf
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2023-02-24 10:26:16 -0500

    day12 part 2

--- day12.jl
+++ day12.jl
@@ -49,14 +49,14 @@ function get_neighbours(curr_min, n, m)
     return(neighbours)
 end
 
-function dijkstra(M::Matrix{Int}, end_node)
+function dijkstra(M::Matrix{Int}, start_node)
     (n, m) = size(M)
 
     dis = zeros(n, m)
     curr_node = []
     for i in 1:n
         for j in 1:m
-            if M[i, j] == 0
+            if M[i, j] == 27
                 dis[i, j] = 0
                 curr_node = (i, j)
             else
@@ -68,12 +68,12 @@ function dijkstra(M::Matrix{Int}, end_node)
     visited = []
     unvisited = Tuple.(findall(a-> isa(a, Number), dis))
 
-    while dis[end_node] == Inf
+    while dis[start_node] == Inf
         neighbours = get_neighbours(curr_node, n, m)
         for node in neighbours
             i = node[1]
             j = node[2]
-            if M[i, j] <= M[curr_node...]+1
+            if M[i, j] >= M[curr_node...]-1
                 min_dis = min(dis[i, j], dis[curr_node[1], curr_node[2]]+1)
                 dis[i, j] = min_dis
             end
@@ -87,7 +87,9 @@ function dijkstra(M::Matrix{Int}, end_node)
 end
 
 
-#display(mat)
-s = findall(a->a ==27, mat)[1]
+s = findall(a->a ==0, mat)[1]
 dis = dijkstra(mat, s)
 println("Part 1: ", dis[s])
+a = findall(a->a ==1, mat)
+part2 = minimum([dis[i] for i in a])
+println("Part 2: ", part2)
